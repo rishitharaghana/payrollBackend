64c41ffe4e9df4e5124cb6b62c83fd209bb251df
rishitharaghana <jrishitha2001@gmail.com>
2025-08-11 10:29:41 +0530

    routes

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const employeeRouter = require('./routes/employeeRoutes');
 const departmentRouter = require('./routes/departmentRoutes');
 const attendanceRouter = require('./routes/attendanceRoutes');
 const leaveRouter = require('./routes/leaveRoutes');
-const payrollRouter = require('./routes/payrollRoutes')
+const payrollRouter = require('./routes/payrollRoutes');
+const deptHeadRouter = require('./routes/deptHeadRoutes');
+const hrRouter = require('./routes/hrRoutes')
+
 
 app.use(cors());
 app.use(express.json());
@@ -23,7 +26,8 @@ app.use('/api', departmentRouter);
 app.use('/api', attendanceRouter);
 app.use('/api', leaveRouter);
 app.use('/api', payrollRouter);
-
+app.use('/api', deptHeadRouter);
+app.use('/api', hrRouter)
 
 const port = 3007;
 app.listen(port, () => {
--- routes/hrRoutes.js
+++ routes/hrRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {authenticationToken} = require('../middleware/authenticate');
 const { getHRs, getHRById, updateHR, deleteHR } = require('../controllers/hrController');
+const { authenticateToken } = require('../middleware/authenticate');
 
-router.get('/hr', authenticationToken,getHRs);
-router.get('/hr/:id', authenticationToken, getHRById);
-router.put('/hr/:id', authenticationToken,updateHR);
-router.delete('/hr/:id', authenticationToken,deleteHR);
+router.get('/hr', authenticateToken,getHRs);
+router.get('/hr/:id', authenticateToken, getHRById);
+router.put('/hr/:id', authenticateToken,updateHR);
+router.delete('/hr/:id', authenticateToken,deleteHR);
 
 module.exports = router;
 
